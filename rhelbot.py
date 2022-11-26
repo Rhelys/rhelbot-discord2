52d5ea41e32c93081aa2a0a4867f415a174832bc
Rhelys <qlverfaillie@gmail.com>
2022-11-26 08:45:15 -0800

    Formatting & addition of a quantity value for the donate command

--- rhelbot.py
+++ rhelbot.py
@@ -1,14 +1,16 @@
 import random
-
 import discord
 from discord import app_commands
 import logging
+from typing import Optional
 
 # Setting up logs
-rhelbot_logs = logging.getLogger('discord')
+rhelbot_logs = logging.getLogger("discord")
 rhelbot_logs.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='rhelbot.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(filename="rhelbot.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
 rhelbot_logs.addHandler(handler)
 
 intents = discord.Intents.default()
@@ -34,13 +36,15 @@ rhelbot = RhelbotClient(intents=intents)
 
 @rhelbot.event
 async def on_ready():
-    print(f'Rhelbot has connected to Discord!\n\n')
-    print(f'{rhelbot.user} is connected to the following Discord servers:\n')
+    print(f"Rhelbot has connected to Discord!\n\n")
+    print(f"{rhelbot.user} is connected to the following Discord servers:\n")
     for guild in rhelbot.guilds:
-        print(f'{guild.name} (id: {guild.id})\n')
+        print(f"{guild.name} (id: {guild.id})\n")
 
 
-@rhelbot.tree.command(description='Displays the number of unique entries in the Starlight giveaway')
+@rhelbot.tree.command(
+    description="Displays the number of unique entries in the Starlight giveaway"
+)
 async def starlightcount(interaction: discord.Interaction):
     await interaction.response.defer()
     channel = rhelbot.get_channel(952012406341648454)
@@ -50,11 +54,13 @@ async def starlightcount(interaction: discord.Interaction):
     for reaction in message.reactions:
         async for user in reaction.users():
             contestants.add(user)
-    await interaction.followup.send(f'{len(contestants)} have entered the giveaway')
+    await interaction.followup.send(f"{len(contestants)} have entered the giveaway")
 
 
-@rhelbot.tree.command(description='Selects a winner from the entries in the Starlight giveaway')
-@app_commands.checks.has_any_role('Waltz Leadership (Flare)', 'Amplifier')
+@rhelbot.tree.command(
+    description="Selects a winner from the entries in the Starlight giveaway"
+)
+@app_commands.checks.has_any_role("Waltz Leadership (Flare)", "Amplifier")
 async def starlightwinner(interaction: discord.Interaction):
     await interaction.response.defer()
     channel = rhelbot.get_channel(952012406341648454)
@@ -67,21 +73,31 @@ async def starlightwinner(interaction: discord.Interaction):
 
     people = list(contestants)
     winner = random.choice(people)
-    await interaction.followup.send(f'@{winner} has been selected as the winner!')
+    await interaction.followup.send(f"@{winner} has been selected as the winner!")
 
 
-@rhelbot.tree.command(description='Adds a donated item to the item list')
-@app_commands.checks.has_any_role('Waltz Leadership (Flare)', 'Amplifier')
-@app_commands.describe(item='Item donated', member='Person who donated the item')
-async def donate(interaction: discord.Interaction, item: str, member: str):
+@rhelbot.tree.command(description="Adds a donated item to the item list")
+@app_commands.checks.has_any_role("Waltz Leadership (Flare)", "Amplifier")
+@app_commands.describe(
+    item="Item donated",
+    member="Person who donated the item",
+    quantity="(Optional) Number of items donated. Defaults to 1 if not provided"
+)
+async def donate(
+    interaction: discord.Interaction, quantity: Optional[int], item: str, member: str
+):
     await interaction.response.defer()
 
-    itemfile = open("items.txt", "a")
-    itemfile.write(f'{item},{member}')
+    quantity = quantity or 1
+
+    itemfile = open("G:/My Drive/waltz_starlight_donations.txt", "a")
+    itemfile.write(f"{item},{member},{quantity}")
     itemfile.write("\n")
     itemfile.close()
 
-    await interaction.followup.send(f'Recorded {item} donated by {member}\n')
+    await interaction.followup.send(
+        f"Recorded {quantity} {item}(s) donated by {member}\n"
+    )
 
 
 # Starting the bot
